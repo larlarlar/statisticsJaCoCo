61db4f7e284af5bbedc625a7e709a12ecbddff83
larlarlar <larisa.gurina.s@gmail.com>
2021-02-06 14:28:59 +0300

    renamed project

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -8,7 +8,7 @@ class StatisticsServiceTest {
 
   @Test
   void findMax() {
-    StatisticsService service = new StatisticsService();
+    StatisticService service = new StatisticService();
 
     long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
     long expected = 12;
